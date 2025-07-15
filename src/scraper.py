a1f7232f2601bcd1cdf7658b7ff16241f9988839
amos zohar <azamoszohar@gmail.com>
2025-07-15 22:30:13 +0300

    added saving to csv function

--- src/scraper.py
+++ src/scraper.py
@@ -2,11 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import argparse
+import os
 
 SOURCE_URL = "https://news.ycombinator.com/"
 TR_CLASS_NAME = "athing submission"
 TITLE_A_CLASS_NAME = "titleline"
 USER_CLASS_NAME = "hnuser"
+
+debug_mode = False
 
 NA = "NA"
 
@@ -33,6 +36,7 @@ DEFAULT_MAX_SCORE = 10000
 DEFAULT_SKIP_PAGES = {}
 
 STATIC_FILE_PATH = "./htmlTestPages"
+OUTPUT_FILE_PATH = "./output/result.csv"
 HTM_SUFFIX = ".htm"
 HTML_SUFFIX = ".html"
 
@@ -40,9 +44,11 @@ HELP_NUM_POST = "Maximum numbers of posts to scrape"
 HELP_MIN_SCORE = "Filter: score >= "
 HELP_MAX_SCORE = "Filter: score <="
 HELP_SKIP_PAGES = "Specify a list of pages to skip, in the format --pages = p1,p2,p3"
+HELP_DEBUG = "Allows debug mode"
 
 def dbgprint(msg):
-    print(f"[DEBUG] {msg}")
+    if(debug_mode):
+        print(f"[DEBUG] {msg}")
 
 def errprint(msg):
     print(f"[ERROR] {msg}")
@@ -60,7 +66,7 @@ def local_parser(file_path):
 #TODO: add filtering function
 
 def extract_from_soup(soup,p_num):
-    extracted_data = {}
+    extracted_data = []
     for tr in soup.find_all(TR, class_ = TR_CLASS_NAME):
         id = tr[ID]
         title_td = tr.find(SPAN,class_ = TITLE_A_CLASS_NAME)
@@ -85,7 +91,6 @@ def extract_from_soup(soup,p_num):
             num_comments = NA
             try:
                 num_comments_str = comments_str.split(COMMENTS_SPLITTER)[0]#TODO: remove magic number
-                # dbgprint(num_comments_str)
                 num_comments = int(num_comments_str)
             except Exception as e:
                 errprint(e)
@@ -93,15 +98,29 @@ def extract_from_soup(soup,p_num):
             author_name = NA
             author_link = NA
 
-        extracted_data[id] = {TITLE:title,
-                               URL: url,
-                               AUTHOR:{"name":author_name,"url":author_link},
-                               POINTS: score,
-                               NUMBER_OF_COMMENTS: num_comments,
-                               PAGE_NUMBER: p_num}
+        extracted_data.append(
+            {
+            TITLE:title,
+            URL: url,
+            AUTHOR:{"name":author_name,"url":author_link},
+            POINTS: score,
+            NUMBER_OF_COMMENTS: num_comments,
+            PAGE_NUMBER: p_num
+            })
     return extracted_data
 
+def save_to_csv(posts_list):
+    os.makedirs(os.path.dirname(OUTPUT_FILE_PATH),exist_ok=True)
+    fields = [TITLE,URL,AUTHOR,POINTS,NUMBER_OF_COMMENTS,PAGE_NUMBER]
 
+    is_file_exists = os.path.exists(OUTPUT_FILE_PATH) and os.path.getsize(OUTPUT_FILE_PATH) > 0
+
+    with open(OUTPUT_FILE_PATH,'a',newline='') as csvfile:
+        writer = csv.DictWriter(csvfile,fieldnames=fields)
+        if not is_file_exists:
+            writer.writeheader()
+        for post_dict in posts_list:
+            writer.writerow(post_dict)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -109,31 +128,39 @@ def parse_args():
     parser.add_argument("--min_score",type=int,default=DEAULT_MIN_SCORE,help=HELP_MIN_SCORE)
     parser.add_argument("--max_score",type=int,default=DEFAULT_MAX_SCORE,help=HELP_MAX_SCORE)
     parser.add_argument("--list_string",type=str,default="",help=HELP_SKIP_PAGES)
+    parser.add_argument("--debug",action="store_true",help=HELP_DEBUG)
     return parser.parse_args()
 
 
 
 if __name__ == "__main__":
+    print("Trying to prase arguments...")
     args = parse_args()
-    dbgprint("Trying to prase arguments...")
     try:
-        # dbgprint(args.num_post)
-        # dbgprint(args.min_score)
-        # dbgprint(args.max_score)
+        debug_mode = args.debug
+        dbgprint("DEBUG MODE ON!")
+        dbgprint(args.num_post)
+        dbgprint(args.min_score)
+        dbgprint(args.max_score)
         s = args.list_string
-        # dbgprint(s)
+        dbgprint(s)
         skip_pages = [int(i) for i in s.split(",") if i]
-        # dbgprint(f"The skip list is: {skip_pages}")
+        dbgprint(f"The skip list is: {skip_pages}")
     except Exception as e:
         errprint(f"something went wrong with argument parsing: {e}")
     dbgprint("After argument parsing...")
-    extracted_data = {}
+    extracted_data = []
     try:
         for i in range(1,2):
-            x = local_parser(STATIC_FILE_PATH+"/"+str(i)+HTM_SUFFIX)
-            extracted_data[str(i)] = extract_from_soup(x,i)
+            i_soup = local_parser(STATIC_FILE_PATH+"/"+str(i)+HTM_SUFFIX)
+            extracted_data += extract_from_soup(i_soup,i)
     except Exception as e:
-        errprint(f"something went wrong with scraping: {e}")
+        errprint(f"Error during scraping: {e}")
 
     dbgprint(f"Extracted data: {extracted_data}")
+    print("Writing data to csv...")
+    try:
+        save_to_csv(extracted_data)
+    except Exception as e:
+        errprint(f"Saving to CSV failed: {e}")
     print("DONE")
